452ee927482e03bb5b75c7206f3640ebed52c697
frodrigueza <feliperodriguezarteaga@gmail.com>
2015-01-22 11:28:22 -0300

    mejoras

--- app/models/task.rb
+++ app/models/task.rb
@@ -578,7 +578,7 @@ class Task < ActiveRecord::Base
 			# si lo piden en recursos
 			else
 				children.each do |c|
-					total_children_value += c.resources_cost
+					total_children_value += c.resources_cost_from_children
 					total_children_value_extolled += c.expected_progress_function(date, in_resources) * c.resources_cost_from_children
 				end
 			end
