31fe6c6b585cac9f9482e41a134ee8151e76e6e2
frodrigueza <feliperodriguezarteaga@gmail.com>
2015-04-29 11:39:43 -0300

    mejoras

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.all.sort_by { |u| u.id }
   end
 
   def new
